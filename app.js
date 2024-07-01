f47665d76aaeb38a2a611631b1aed9c4903b8eb1
Abdulloh-272 <167763951+Abdulloh-272@users.noreply.github.com>
2024-07-01 23:00:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+function navbar() {
+    const nav = document.querySelector('.nav')
+    nav.classList.remove('hidden')
+}
+
+function back() {
+    const back = document.querySelector('.nav')
+    back.classList.add('hidden')
+}
